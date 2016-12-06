b4d7fe662e90fe7e06ce57d74a496d07b9e9050f
Daniel Sendzik <d.sendzik@gmail.com>
2016-12-06 21:53:43 +0100

    Adapted copyq configuration

---
+++ home/.emacs.d/lisp/init-org.el
@@ -0,0 +1,9 @@
+;;; init-org.el -- My org mode configuration.
+;;; Commentary:
+;;; Code:
+
+(setq browse-url-browser-function 'browse-url-generic
+      browse-url-generic-program "vivaldi-stable")
+
+(provide 'init-org)
+;;; init-org.el ends here
