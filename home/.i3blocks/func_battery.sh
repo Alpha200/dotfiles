038e9d3b1623c8893813f49c19d9798bf0a2dc68
Daniel Sendzik <daniel@sendzik.eu>
2019-03-27 15:22:28 +0100

    Disabled emacs on startup and fixed really old bug

--- home/.i3blocks/func_battery.sh
+++ home/.i3blocks/func_battery.sh
@@ -2,4 +2,8 @@
 BATCURRENT=`acpi | awk '$0~/ /{print $4}' | tr -d ','`
 BATTIME=`acpi | awk '$0~/ /{print $5}' | tr -d ','`
 
+if [[ "$BATTIME" == "until" ]]; then
+  BATTIME=""
+fi
+
 echo "$BATCURRENT  $BATTIME"
